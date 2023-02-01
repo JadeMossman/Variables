7b0a050496a03bf160327c683c2bf622310041c6
JadeMossman <123392459+JadeMossman@users.noreply.github.com>
2023-02-01 11:29:01 +0000

    Add files via upload

---
+++ HELLOdave.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+   string name;
+   cout << "What is your name: ";
+   cin >> name;
+
+   cout << "\nHello, " << name << ". I hope you are well today?\n";
+    int age;
+
+    cout << "what is your age: ";
+    cin >> age;
+    cout <<"\nYou are " << age << " years old.\n";
+}
---
+++ piradius.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace std;
+
+int main ()
+{
+    double area, radius = 1.5;
+    area = M_PI * radius * radius;
+    cout << area << "\n";
+}
+
+
