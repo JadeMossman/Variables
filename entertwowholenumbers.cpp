0d9c42f670a9bd5de0d450d07f332eac17db86ce
JadeMossman <123392459+JadeMossman@users.noreply.github.com>
2023-02-01 11:43:31 +0000

    Add files via upload

---
+++ entertwowholenumbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int num1, num2;
+cout << "Enter two whole numbers: ";
+cin >> num1 >> num2;
+cout << " You entered " << num1 << " and " << num2 << "\n";
+}
